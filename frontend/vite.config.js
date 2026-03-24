850d8d2dfe89c0acceb3de2171e1d60bbddb2fad
madhav dhavala <madhavdhavala0@gmail.com>
2026-03-24 13:48:32 +0530

    pushing before pull

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.glb'],
+  optimizeDeps: {
+    include: [
+      '@react-three/drei',
+      '@react-three/fiber',
+      'three',
+      'use-sync-external-store/shim/with-selector'
+    ],
+  },
   server: {
     proxy: {
       '/api-gemini': {
